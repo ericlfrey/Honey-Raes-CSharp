a2adee3956ab8c1327a6bd9fca1abab2187df263
Eric Frey <ericlfrey@gmail.com>
2024-01-31 17:20:54 -0600

    Created Customers Components

---
+++ src/components/customers/CustomerDetails.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CustomerDetails() {
+  return <div>CustomerDetails</div>;
+}
---
+++ src/components/customers/CustomersList.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { getCustomers } from '../../data/customerData';
+import { Table } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+export default function CustomersList() {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    getCustomers().then(setCustomers);
+  }, []);
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Address</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {customers.map(c => (
+          <tr key={`customer-${c.id}`}>
+            <th scope="row">{c.id}</th>
+            <td>{c.name}</td>
+            <td>{c.address}</td>
+            <td>
+              <Link to={`${c.id}`}>Details</Link>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
