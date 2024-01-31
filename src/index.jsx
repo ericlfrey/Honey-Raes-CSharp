f314dd3a905abb20487fcc763f25d9eeae8541d9
Eric Frey <ericlfrey@gmail.com>
2024-01-31 17:26:31 -0600

    Edited index.jsx and app.jsx to handle routing.

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ServiceTickets from "./components/tickets/ServiceTickets";
-import TicketsList from "./components/tickets/TicketsList";
-import TicketDetails from "./components/tickets/TicketDetails";
-import CreateTicket from "./components/tickets/CreateTicket";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ServiceTickets from './components/tickets/ServiceTickets';
+import TicketsList from './components/tickets/TicketsList';
+import TicketDetails from './components/tickets/TicketDetails';
+import CreateTicket from './components/tickets/CreateTicket';
+import Customers from './components/customers/Customers';
+import CustomersList from './components/customers/CustomersList';
+import CustomerDetails from './components/customers/CustomerDetails';
+import CreateCustomer from './components/customers/CreateCustomer';
+import Employees from './components/employees/Employees';
+import EmployeesList from './components/employees/EmployeesList';
+import EmployeeDetails from './components/employees/EmployeeDetails';
+import CreateEmployee from './components/employees/CreateEmployee';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
@@ -19,9 +27,19 @@ root.render(
           <Route path=":id" element={<TicketDetails />} />
           <Route path="create" element={<CreateTicket />} />
         </Route>
+        <Route path="customers" element={<Customers />}>
+          <Route index element={<CustomersList />} />
+          <Route path=":id" element={<CustomerDetails />} />
+          <Route path="create" element={<CreateCustomer />} />
+        </Route>
+        <Route path="employees" element={<Employees />}>
+          <Route index element={<EmployeesList />} />
+          <Route path=":id" element={<EmployeeDetails />} />
+          <Route path="create" element={<CreateEmployee />} />
+        </Route>
       </Route>
     </Routes>
-  </BrowserRouter>,
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
