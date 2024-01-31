e9e39d81828f50b415ab3e927a6997d3b24d5ba3
Eric Frey <ericlfrey@gmail.com>
2024-01-31 17:09:27 -0600

    Changed target again. Wasnt working with https.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(() => {
       open: true,
       proxy: {
         "/api": {
-          target: "https://localhost:5000",
+          target: "http://localhost:5000",
           changeOrigin: true,
           secure: false,
         },
