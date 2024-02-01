7afc25e10bbc43152e7c045beb0fa5cd7f7ed560
Eric Frey <ericlfrey@gmail.com>
2024-02-01 09:12:29 -0600

    Created an EmployeeDetails component. Tested in app.

--- src/components/employees/EmployeeDetails.jsx
+++ src/components/employees/EmployeeDetails.jsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Table } from 'reactstrap';
+import { getEmployee } from '../../data/employeeData';
 
 export default function EmployeeDetails() {
-  return <div>EmployeeDetails</div>;
+  const { id } = useParams();
+
+  const [employee, setEmployee] = useState(null);
+
+  useEffect(() => {
+    getEmployee(id).then(setEmployee);
+  }, []);
+
+  if (!employee) {
+    return null;
+  }
+
+  return (
+    <>
+      <Table>
+        <tbody>
+          <tr>
+            <th scope="row">Employee</th>
+            <td>{employee.name}</td>
+          </tr>
+          <tr>
+            <th scope="row">Specialty</th>
+            <td>{employee.specialty}</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
 }
