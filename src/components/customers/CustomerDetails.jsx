1a8465be7780aa876edd5a164fb8eb7f0199f50f
Eric Frey <ericlfrey@gmail.com>
2024-02-01 09:10:02 -0600

    Created a CustomerDetails component. Tested in app.

--- src/components/customers/CustomerDetails.jsx
+++ src/components/customers/CustomerDetails.jsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Table } from 'reactstrap';
+import { getCustomer } from '../../data/customerData';
 
 export default function CustomerDetails() {
-  return <div>CustomerDetails</div>;
+  const { id } = useParams();
+
+  const [customer, setCustomer] = useState(null);
+
+  useEffect(() => {
+    getCustomer(id).then(setCustomer);
+  }, []);
+
+  if (!customer) {
+    return null;
+  }
+
+  return (
+    <>
+      <Table>
+        <tbody>
+          <tr>
+            <th scope="row">Customer</th>
+            <td>{customer.name}</td>
+          </tr>
+          <tr>
+            <th scope="row">Address</th>
+            <td>{customer.address}</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
 }
