11b2c912e44cae9a57b6b6c0bdc88fae2b8a41c6
Eric Frey <ericlfrey@gmail.com>
2024-02-02 09:44:33 -0600

    Used the useParams hook to get access to the ticket number

--- src/components/employees/AssignEmployee.jsx
+++ src/components/employees/AssignEmployee.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function AssignEmployee() {
+  const { id } = useParams();
   return (
     <>
-      <h2>Assign Employee</h2>
+      <h2>Assign Employee to ticket {id}</h2>
     </>
   );
 }
